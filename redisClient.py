794d59efd13d17d71764910b186f8438bffaf8ae
bernardk23 <bernardkissi@live.com>
2022-02-04 11:46:11 +0000

    fixed redis client initialization error

--- redisClient.py
+++ redisClient.py
@@ -1,7 +1,22 @@
 import redis
+from itertools import zip_longest
 
-class RedisClient:
-    def __init__(self, host:str = 'localhost', port:int = 6379, db:int = 0):
-        self.host = host
-        self.port = port
-        self.client = redis.Redis(host=self.host, port=self.port, db=0)
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class RedisClient(metaclass=Singleton):
+    def __init__(self, host:str = 'localhost', port:int = 6379, db=0, decode_responses=True):
+        self.__host = host
+        self.__port = port
+        self.client = redis.Redis(host=self.__host, port=self.__port, db=0)
+        self.pub = self.client.pubsub()
+        self.data = {}
+        # self.pub.run_in_thread(sleep_time=0.01)
+
+
+
+
